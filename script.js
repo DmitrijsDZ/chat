ce552245d834c22fea1c65097f0408c223ebc462
DmitrijsDZ <92847190+DmitrijsDZ@users.noreply.github.com>
2022-03-18 12:08:57 +0200

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+let zina=document.querySelector('.manaZina')
+let zinas=document.querySelector('.chataZinas')
+
+function sutitZinu()
+{
+    console.log("funkcija darbojas");
+    zinas.innerHTML= zinas.innerHTML+"<br/>"+zina.value;
+}
+ async function ielaidetChataZinas()
+ {
+let datiNoServera= await fetch('chatazinas.txt');
+let dati= await datiNoServera.text();
+zinas.innerHTML=dati;
+ }
+ setInterval(ielaidetChataZinas,1000)
